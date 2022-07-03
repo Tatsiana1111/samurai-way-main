fecf86e74d22cd4bf88964419b78d39c27bcaebf
Tatsiana1111 <tanyashket@gmail.com>
2022-07-03 20:00:20 +0300

    lesson 20

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import style from "./Navbar.module.css"
+import {NavLink} from "react-router-dom";
 
 const Navbar = () => {
     return (
         <nav className={style.nav}>
-            <div className={style.item}><a href="/profile">Profile</a></div>
-            <div className={style.item}><a href="/dialogs">Messages</a></div>
-            <div className={style.item}><a href='/news'>News</a></div>
-            <div className={`${style.item} ${style.active}`}><a href='/music'>Music</a></div>
-            <div className={style.item}><a href='/settings'>Settings</a></div>
+            <div className={style.item}><NavLink to="/profile" activeClassName={style.activeLink}>Profile</NavLink>
+            </div>
+            <div className={style.item}><NavLink to="/dialogs" activeClassName={style.activeLink}>Messages</NavLink>
+            </div>
+            <div className={style.item}><NavLink to='/news' activeClassName={style.activeLink}>News</NavLink></div>
+            <div className={`${style.item} ${style.active}`}><NavLink to='/music'
+                                                                      activeClassName={style.activeLink}>Music</NavLink>
+            </div>
+            <div className={style.item}><NavLink to='/settings' activeClassName={style.activeLink}>Settings</NavLink>
+            </div>
         </nav>
     );
 };

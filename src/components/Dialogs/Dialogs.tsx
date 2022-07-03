67ef5bf5466fab46e43e4bcd9878849a7cb744d5
Tatsiana1111 <tanyashket@gmail.com>
2022-07-03 20:24:20 +0300

    lesson 21

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,23 @@ import style from './Dialogs.module.css'
 
 export const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={style.dialogs}>
+            <div className={style.dialogItems}>
+                <div className={style.dialog + ' ' + style.active}>Tanya</div>
+                <div className={style.dialog}>Alex</div>
+                <div className={style.dialog}>Vika</div>
+                <div className={style.dialog}>Dima</div>
+                <div className={style.dialog}>Alena</div>
+                <div className={style.dialog}>Nick</div>
+            </div>
+            <div className={style.messages}>
+                <div className={style.message}>Hello</div>
+                <div className={style.message}>Where are you now??</div>
+                <div className={style.message}>Good of you, baby))</div>
+                <div className={style.message}>What the bloody hell is this???</div>
+                <div className={style.message}>I'm okay, thanks</div>
+                <div className={style.message}>Very well!</div>
+            </div>
         </div>
     );
 };

37f3c43ff2ad24338b085acc666720c771c3e2d6
Tatsiana1111 <tanyashket@gmail.com>
2022-07-03 19:43:23 +0300

    lesson 19

---
+++ src/components/Settings/Settings.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import style from './Settings.module.css'
+
+export const Settings = () => {
+    return (
+        <div>
+            Settings
+        </div>
+    );
+};
+

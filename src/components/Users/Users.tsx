5ab97199e5a21aa55eb8c9a5dbd184a8b6292fa9
Tatsiana1111 <tanyashket@gmail.com>
2022-09-23 11:03:51 +0300

    lesson 54

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -7,8 +7,8 @@ import userIcon from '../../assets/images/userIcon.png'
 type UsersPropsType = MapStatePropsType & MapDispatchPropsType
 
 export class Users extends React.Component<UsersPropsType, UsersPropsType> {
-    constructor(props: UsersPropsType) {
-        super(props);
+    
+    componentDidMount() {
         axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => {
             this.props.setUsers(
                 response.data.items
